fd9bc623a1066afc2a2195db3bb97cef41d5f84c
MS2 <67571006+MarleeSs@users.noreply.github.com>
2021-11-25 02:18:29 +0700

    Add files via upload

---
+++ Variable/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        // Assignment
+        int a = 10;
+        System.out.println(">>>a Bervariable " + a);
+
+        // Deklarasi
+        int b;
+        b = 20;
+        System.out.println(">>>b Bervariable " + b);
+    }
+}
