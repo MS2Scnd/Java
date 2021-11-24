bb0c6df9765119b7502cc03edc808c329bf81b10
MS2 <67571006+MarleeSs@users.noreply.github.com>
2021-11-24 18:43:13 +0700

    Update Print.java

--- Print/Print.java
+++ Print/Print.java
@@ -1,3 +1,8 @@
+// Apa itu java? 
+// Java adalah bahasa pemrogramman yang dulunya bernama GreeProject dipegang oleh SUN Microsystem pada tahun 1991. Dan pada tahun 2009, Oracle membeli SUN Microsystem.
+// semua asset SUN termasuk project JAVA berpindah kepemilikan menjadi milik Oacle sampai saat kini 2021.
+// Java juga bisa Multi-Platform, Mobile Application, Server Side Application, dan System Emmbedded Robotics.
+
 public class Print
 {
         public static void main(String[] args) throws Exception
