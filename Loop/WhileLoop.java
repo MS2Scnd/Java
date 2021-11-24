fdb78418cffe21e3bd8a9fa2abc0fa371fd9fa83
MS2 <67571006+MarleeSs@users.noreply.github.com>
2021-11-25 02:19:59 +0700

    Update WhileLoop.java

--- Loop/WhileLoop.java
+++ Loop/WhileLoop.java
@@ -1,25 +1,38 @@
 /*
  # Algoritmik
 
- Start
+ mulai Loop
  awal = 1
- ketika awala <= 5
- awal ditambahkan 1, lalu
- tampilkan "UBP Karawang"
- ketika awal sudah tepat dari 5
- keluar dari loop lalu tampilkan "Keluar dari Loop"
- Selesai.
-
+ bila a kurang dari 5
+ tampilkan "Algoritma" dengan tambahankan variable a
+ bila variable a masih kurang dari 5, variable a ditambahkan lagi dengan variable a
+ ketika variable a sudah mencapai targetnya yaitu kurang dari samadengan 5
+ tampilkan "Loop Selesai"
+ loop selesai
  */
 
+//--------------------------------------------//
 
+// mulai Loop
 public class WhileLoop {
     public static void main(String[] args) {
-        int awal = 1;
-        while (awal <= 5){
-            System.out.println("UBP Karawang " + awal);
-            awal++;
+
+        System.out.println("Mulai Loop");
+
+// awal = 1
+        int a = 1;
+// bila a kurang dari 5 -> (a <= 5)
+        while (a <= 5){
+
+// tampilkan "Algoritma" dengan tambahankan variable a
+            System.out.println("Algoritma " + a);
+
+// bila variable a masih kurang dari 5, variable a ditambahkan lagi dengan variable a -> (a++)
+            a++;
+
+// ketika variable a sudah mencapai targetnya yaitu kurang dari samadengan 5 ->(<=5)
+// tampilkan "Loop Selesai" lalu selesai
         }
-        System.out.println("Keluar dari Loop");
+        System.out.println("Loop Selesai");
     }
 }
