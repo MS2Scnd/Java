f41789ac240b2ca06e863dd3f575ca0717216263
MS2 <67571006+MarleeSs@users.noreply.github.com>
2021-11-27 00:01:35 +0700

    Update Print.java

--- PrintDanPenjelasan/Print.java
+++ PrintDanPenjelasan/Print.java
@@ -7,7 +7,7 @@
 
 public class Print
 {
-        public static void main(String[] args) throws Exception
+        public static void main(String[] args)
         {
             System.out.print("Java");
         }
